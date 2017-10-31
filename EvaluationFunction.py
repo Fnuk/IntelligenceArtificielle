f88698d037ab42ccc429861a6e21ac018c179db1
AntoinePicard <antoine.picard@outlook.fr>
2017-10-31 12:09:42 +0100

    Ajout closeBridge dans evalfunction

--- EvaluationFunction.py
+++ EvaluationFunction.py
@@ -1,19 +1,18 @@
-def EvaluationFunction(plateau, position, joueur):
+def evaluationfunction(plateau, position, joueur):
+    """fonction principale"""
     val = 0
     # premier coup ? milieu !
     if plateau.count("0") == 121:
         if position[0] == position[1] == 5:
-            val += 4
-    # test si bridge
-    # retourne entre 0 (bridge tous adverses) et 12/14 (bridge tous à nous)
+            val += 10
     val += bridge(plateau, position, joueur)
-    # test si echelle
-    # retourne entre 0 et 
+    val += close_bridge(plateau, position, joueur)
     val += ladder(plateau, position, joueur)
     return val
 
 
 def bridge(plateau, position, joueur):
+    """test si bridge / retourne entre 0 et 12/14"""
     val = 0
     # bridge vertical
     if plateau[position[0] + 1, position[1] - 2] == joueur:
@@ -58,5 +57,34 @@ def bridge(plateau, position, joueur):
 
 
 def ladder(plateau, position, joueur):
+    """test si echelle / retourne entre 0 et """
     val = 0
+
+    return val
+
+
+def close_bridge(plateau, position, joueur):
+    """test si fermeture d'un bridge / retourne entre 0 et """
+    val = 0
+    # bridge vertical
+    if plateau[position[0] + 1, position[1] - 1] == plateau[position[0], position[1] + 1] == joueur:
+        if joueur == -1:
+            val += 3
+        else:
+            val += 2
+    if plateau[position[0], position[1] - 1] == plateau[position[0] - 1, position[1] + 1] == joueur:
+        if joueur == -1:
+            val += 3
+        else:
+            val += 2
+    # bridge /
+    if plateau[position[0] + 1, position[1]] == plateau[position[0] - 1, position[1] + 1] == joueur:
+        val += 2
+    if plateau[position[0] + 1, position[1] - 1] == plateau[position[0] - 1, position[1]] == joueur:
+        val += 2
+    # bridge \
+    if plateau[position[0], position[1] + 1] == plateau[position[0] + 1, position[1]] == joueur:
+        val += 2
+    if plateau[position[0] - 1, position[1]] == plateau[position[0], position[1] + 1] == joueur:
+        val += 2
     return val
